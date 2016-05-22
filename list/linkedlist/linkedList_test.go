d4039bb20e2e78e14ed7a336e5a1e76f7109c27b
name <freiny50@gmail.com>
2016-05-22 05:04:34 -0500

    Update linkedlist_test.go to use ftest Assert() for cleaner testing

--- list/linkedlist/linkedList_test.go
+++ list/linkedlist/linkedList_test.go
@@ -4,61 +4,56 @@ import (
 	"testing"
 
 	"github.com/freiny/go-ds/list/linkedlist"
+	"github.com/freiny/go-util/ftest"
 )
 
 func TestNew(t *testing.T) {
 	l := linkedlist.New()
-	tests := []bool{
-		l.Head() == nil,
-		l.Tail() == nil,
+
+	tests := []ftest.Test{
+		{0, l.Head(), (*linkedlist.Element)(nil)},
+		{1, l.Tail(), (*linkedlist.Element)(nil)},
 	}
-	msg := []string{
-		"l.Head() == nil",
-		"l.Tail() == nil",
-	}
-	for i, pass := range tests {
-		if !pass {
-			t.Errorf("Append(1) test failed: %q", msg[i])
-		}
-	}
+	ftest.Assert(t, "New()", tests)
 
 }
 
 func TestAppend(t *testing.T) {
 	l := linkedlist.New()
 	l.Append(1)
-	tests := []bool{
-		l.Head().Value == 1,
-		l.Tail().Value == 1,
+	tests := []ftest.Test{
+		{0, l.Head().Value, 1},
+		{1, l.Tail().Value, 1},
 	}
-	msg := []string{
-		"l.Head().Value == 1",
-		"l.Tail().Value == 1",
-	}
-	for i, pass := range tests {
-		if !pass {
-			t.Errorf("Append(1) test failed: %q", msg[i])
-		}
-	}
+	ftest.Assert(t, "Append(1)", tests)
 
 	l.Init()
 	l.Append(1)
 	l.Append(2)
 	l.Append(3)
-	tests = []bool{
-		l.Head().Value == 1,
-		l.Head().Next().Value == 2,
-		l.Tail().Value == 3,
+	tests1 := []ftest.Test{
+		{0, l.Head().Value, 1},
+		{1, l.Head().Next().Value, 2},
+		{2, l.Tail().Value, 3},
 	}
-	msg = []string{
-		"l.Head().Value == 1",
-		"l.Head().Next().Value == 2",
-		"l.Tail().Value == 3",
+	ftest.Assert(t, "Append(1,2,3)", tests1)
+}
+
+func TestInsertAfter(t *testing.T) {
+	l := linkedlist.New()
+
+	l.InsertAfter(1, nil)
+	l.InsertAfter(5, l.Tail())
+	e := l.InsertAfter(3, l.Head())
+	l.InsertAfter(4, e)
+
+	tests := []ftest.Test{
+
+		{0, l.Head().Value, 1},
+		{1, l.Head().Next().Value, 3},
+		{2, l.Head().Next().Next().Value, 4},
+		{3, l.Tail().Value, 5},
 	}
-	for i, pass := range tests {
-		if !pass {
-			t.Errorf("Append(1...3) test failed: %q", msg[i])
-		}
-	}
+	ftest.Assert(t, "InsertAfter(...1,3,4,5)", tests)
 
 }
