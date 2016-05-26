affa3b9a0ec2c832825436fc3ae3204d97ee4cae
name <freiny50@gmail.com>
2016-05-26 00:50:04 -0500

    Update binaryTree.go: insertRight, insertLeft insert tree objects

--- tree/bintree/binaryTree.go
+++ tree/bintree/binaryTree.go
@@ -29,28 +29,38 @@ func (t *Tree) Right() *Tree {
 	return t.right
 }
 
+// insertLeft inserts new tree between parent node and left child
+// returns inserted tree node or returns nil on failed insert
+func (t *Tree) insertLeft(new *Tree) *Tree {
+	if t.left == nil {
+		t.left = new
+	} else {
+		new.left = t.left
+		t.left = new
+	}
+	return new
+}
+
 // InsertLeft inserts new tree with "value" between parent node and left child
 // returns inserted tree node or returns nil on failed insert
 func (t *Tree) InsertLeft(value interface{}) *Tree {
-	new := Tree{Value: value}
-	if t.left == nil {
-		t.left = &new
+	return t.insertLeft(&Tree{Value: value})
+}
+
+// insertRight inserts new tree between parent node and right child
+// returns inserted tree node or returns nil on failed insert
+func (t *Tree) insertRight(new *Tree) *Tree {
+	if t.right == nil {
+		t.right = new
 	} else {
-		new.left = t.left
-		t.left = &new
+		new.right = t.right
+		t.right = new
 	}
-	return &new
+	return new
 }
 
 // InsertRight inserts new tree with "value" between parent node and left child
 // returns inserted tree node or returns nil on failed insert
 func (t *Tree) InsertRight(value interface{}) *Tree {
-	new := Tree{Value: value}
-	if t.right == nil {
-		t.right = &new
-	} else {
-		new.right = t.right
-		t.right = &new
-	}
-	return &new
+	return t.insertRight(&Tree{Value: value})
 }
